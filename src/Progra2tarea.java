2943c9a67cc6d8381e1b7a39a680096f4f613477
DiegoVelasq98 <89673799+DiegoVelasq98@users.noreply.github.com>
2022-07-22 19:40:19 -0600

    TAREA_2

---
+++ src/Progra2tarea.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Progra2tarea {
+
+
+    public static void main(String[] args) {
+
+        String nombre;
+
+        Scanner consola =new Scanner(System.in);
+
+        System.out.println("Ingresa tu nombre");
+
+        nombre= consola.nextLine();
+
+        System.out.println("Bienvenido a la clase de Progra 2 2022 " + nombre);
+
+
+    }
+}
